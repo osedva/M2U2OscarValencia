4fe0f095fa2b61645cdcd73297012ad79480e59c
Oscar Eduardo Valencia Valencia <excelvalencia75@gmail.com>
2022-06-28 18:07:43 -0500

    cambio en el archivo tareados.js separador - * / y validar que no reciba numeros menores a 1

--- tareados.js
+++ tareados.js
@@ -32,12 +32,14 @@ btn.addEventListener('click',(e)=>{
        }
       
         let unicos = new Set(arr);
-        if(unicos.size == 4){
+        if(numeA.value < 1 || numeB.value < 1 || numeC.value < 1 || numeD.value < 1){
+            info.innerHTML = "Solo numeros mayores a 0";  
+        }else if(unicos.size == 4){
             var max = Math.max.apply(null, arr);
             var min = Math.min.apply(null, arr);
-            info.innerHTML = "de la lista de numeros " + arr.join(" - ") + "<br>el numero maximo es " + max + "<br>y el numero minimo es " + min;
+            info.innerHTML = "de la lista de numeros " + arr.join(" / ") + "<br>el numero maximo es " + max + "<br>y el numero minimo es " + min;
             document.forms["mayme"].reset();
-            document.forms["mayme"].elements[0].focus();
+            document.forms["mayme"].elements[0].focus(); 
         }else{
             info.innerHTML = "No pueden haber numeros repetidos";
         }       
